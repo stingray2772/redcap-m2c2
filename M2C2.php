7d48c2933d5f5608c1ed309a4aa2154c0b600307
Cory Jackson <cjj5167@psu.edu>
2024-05-22 11:39:20 -0400

    wip

--- M2C2.php
+++ M2C2.php
@@ -1,54 +1,35 @@
 <?php
-// Set the namespace defined in your config file
 namespace PSU\M2C2;
 
 use \REDCap;
 use \Survey;
 
-// Declare your module class, which must extend AbstractExternalModule 
 class M2C2 extends \ExternalModules\AbstractExternalModule
 {
     protected $is_survey = 0;
 
-    protected  static $Tags =  array('@M2C2' => array('description' => 'Basic M2C2 description goes here') );
+    protected static $Tags =  array('@M2C2' => array('description' => 'Basic M2C2 description goes here'));
 
     // Define the JSON field for the M2C2 annotation
-    protected static $M2C2_JSON = array('type', 'trials');
+    protected static $M2C2_JSON = array('type', 'trials', 'trialdata');
 
     function redcap_survey_page($project_id, $record, $instrument, $event_id, $group_id, $survey_hash, $response_id, $repeat_instance) {
         global $Proj;
-        // echo "<pre>";
-        // print_r($Proj);
-        // echo "</pre>";
-
-        // $settings = $this->getProjectSettings();
-        // echo "<pre>";
-        // print_r($settings);
-        // echo "</pre>";
 
         // Get Instrument Data Dictionary
         $instrument_dict_json =  REDCap::getDataDictionary($this->getProjectId(), "json", False, Null, array($instrument));
-        $instrument_dict =  json_decode($instrument_dict_json,true);
+        $instrument_dict =  json_decode($instrument_dict_json, true);
 
-        foreach($instrument_dict as $field){
-            // echo "Field Name: " . $field['field_name'] . "<br>";
-            // echo "Field Type: " . $field['field_type'] . "<br>";
-            // echo "Field Annotation: " . $field['field_annotation'] . "<br>";
-            // echo (($field['field_type'] == "notes") ? "Field type is notes" : "Field type is not notes" ) ."<br>";
-            // echo ((strpos($field['field_annotation'], "@M2C2") !== false) ? "Field annotation is m2c2" : "Field annotation is not m2c2" ) ."<br>";
+        foreach($instrument_dict as $field) {
             if ($field['field_type'] == "notes" && strpos($field['field_annotation'], "@M2C2") !== false) {
                 $isValidM2C2 = false;
-                // Pick out the 
-                //echo "<script>alert('Found M2C2');</script>";
-                // echo "<pre>";
-                // print_r($field);
-                // echo "</pre>";
 
                 // Regular expression to match @M2C2 and capture the JSON part
-                $pattern = '/@M2C2=({.*?})/';
+                //$pattern = '/@M2C2=({.*?})/';
+                $pattern = '/@M2C2=(\{[^}]+\})/';
 
                 // Array to store the extracted JSON
-                $jsonArray = [];
+                $m2c2Settings = [];
 
                 // Perform the regex match
                 if (preg_match($pattern, $field['field_annotation'], $matches)) {
@@ -56,130 +37,128 @@ class M2C2 extends \ExternalModules\AbstractExternalModule
                     $jsonString = $matches[1];
                     
                     // Decode the JSON string into an associative array
-                    $jsonArray = json_decode($jsonString, true);
-                    
+                    $m2c2Settings = json_decode($jsonString, true);
+
+                    echo "M2C2 Settings: <br>";
+                    echo "<pre>";
+                    print_r($m2c2Settings);
+                    echo "</pre>";
+
                     // Check for JSON decoding errors
                     if (json_last_error() === JSON_ERROR_NONE) {
-                        // Verify we have all the correct keys by cross referencing $M2C2_JSON
+                        // Verify we have all the correct keys by cross-referencing $M2C2_JSON
                         $isValidM2C2 = true;
+                        
+                        // Check if all keys in trialdata exist in the dictionary
+                        if (isset($m2c2Settings['trialdata']) && is_array($m2c2Settings['trialdata'])) {
+                            foreach ($m2c2Settings['trialdata'] as $trialNum => $fieldName) {
+                                $hasField = false;
+                                foreach ($instrument_dict as $dictField) {
+                                    if ($dictField['field_name'] == $fieldName) {
+                                        $hasField = true;
+                                        break;
+                                    }
+                                }
+                                
+                                if (!$hasField) {
+                                    $isValidM2C2 = false;
+                                    break;
+                                }
+                            }
+
+                            echo "Checking for trialdata keys in dictionary: <br>";
+                            echo "<pre>";
+                            print_r($m2c2Settings['trialdata']);
+                            echo "</pre>";
+
+                            // display isValidM2C2 true/false
+                            echo "isValidM2C2: " . ($isValidM2C2 ? "true" : "false") . "<br>";
+
+                        } else {
+                            $isValidM2C2 = false; // trialdata is missing or not an array
+                        }
+
+                        // Additional check for top-level keys
                         foreach (self::$M2C2_JSON as $key) {
-                            if (!array_key_exists($key, $jsonArray)) {
+                            if (!array_key_exists($key, $m2c2Settings)) {
                                 $isValidM2C2 = false;
                                 break;
                             }
                         }
                     } else {
-                        $isValidM2C2 = false;
+                        echo "<script>alert('JSON decoding error: " . json_last_error() . "');</script>"; // "0" means "no error"
+                        $isValidM2C2 = false; // JSON decoding error
                     }
                 }
 
-                // echo "isValidM2C2: " . ($isValidM2C2 ? "true" : "false") . "<br>";
                 if ($isValidM2C2) {
-                    // echo "M2C2 Type: " . $jsonArray['type'] . "<br>";
-                    // echo "M2C2 Trials: " . $jsonArray['trials'] . "<br>";
-
-                    // $url = 'https://beta.m2c2kit.com/m2c2kit/ntc/index.html?activity_name=symbol-search';
-                    // $url .= '&n_trials=' . $jsonArray['trials'];
-                    // $url .= '&participant_id=None&session_id=None&study_id=None&api_key=e16a3173-a09e-43b5-81b2-d36369d64fe8';
-
-                    // echo '<div id="overlay-iframe-container" style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; z-index: 9999; background: rgba(0, 0, 0, 0.5);">
-                    //     <iframe id="overlay-iframe" src="' . $url . '" style="height: 100%; width: 100%; border: none;"></iframe>
-                    // </div>';
-
-                    // echo '<style>
-                    // html, body {
-                    //     margin: 0;
-                    //     padding: 0;
-                    //     height: 100%;
-                    //     overflow: hidden; /* Prevents scrolling */
-                    // }
-                    // </style>';
-
-                    // echo '<script>
-                    // function showIframe() {
-                    //     document.getElementById("overlay-iframe-container").style.display = "flex";
-                    // }
-
-                    // function hideIframe() {
-                    //     document.getElementById("overlay-iframe-container").style.display = "none";
-                    // }
-
-                    // document.addEventListener("DOMContentLoaded", function() {
-                    //     showIframe(); // Show iframe when the page loads
-                    // });
-                    // </script>';
-
-                    // echo '<script>
-                    //     var embeddedDataPrefix = "TRIAL_DATA_";
-                    //     window.addEventListener("message", (event) => {
-                    //         console.log("event fired: " + event.data["name"]);
-                    //         if (event.data["name"] === "m2c2kit-trialdone" || event.data["name"] === "newData") {
-                    //             console.log("found new event: " + event.data["name"]);
-                    //             var data = JSON.parse(event.data["data"]);
-                    //             var trial_num = data["trial_index"];
-
-                    //             // Store data into embedded variable
-                    //             // Replace "mtooctoo" with your desired field name
-                    //             $("#mtooctoo").val(JSON.stringify(data));
-                    //             console.log("logging data for trial: " + trial_num);
-
-                    //             // Hide the iframe container
-                    //             $("#overlay-iframe-container").hide();
-                    //         } else {
-                    //             console.log("Uncaught event received in parent window!: ");
-                    //             console.log(event);
-                    //         }
-                    //     });
-                    //     </script>';
-                    if ($isValidM2C2) {
-                        // Construct the URL with the specified number of trials
-                        $url = 'https://beta.m2c2kit.com/m2c2kit/ntc/index.html?activity_name=symbol-search';
-                        $url .= '&n_trials=' . $jsonArray['trials'];
-                        $url .= '&participant_id=None&session_id=None&study_id=None&api_key=e16a3173-a09e-43b5-81b2-d36369d64fe8';
-                    
-                        echo '<div id="overlay-iframe-container" style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; z-index: 9999; background: rgba(0, 0, 0, 0.5);">
-                            <iframe id="overlay-iframe" src="' . $url . '" style="height: 100%; width: 100%; border: none;"></iframe>
-                        </div>';
-                        echo '<script>
-                        var embeddedDataPrefix = "TRIAL_DATA_";
-                        window.addEventListener("message", function(event) {
-                            console.log("event fired: " + event.data.name);
-                            if (event.data.name === "m2c2kit-trial-done" || event.data.name === "newData") {
-                                console.log("found new event: " + event.data.name);
-                                var data = JSON.parse(event.data.data);
-                                var trial_num = data.trial_index;
-                    
-                                // Store data into embedded variable
-                                // Replace "mtooctoo" with your desired field name
-                                $("#mtooctoo").val(JSON.stringify(data));
-                                console.log("logging data for trial: " + trial_num);
-                    
-                                // Hide the iframe container
-                                $("#overlay-iframe-container").hide();
-                            } else if (event.data.name === "m2c2kit-done") {
-                                console.log("m2c2kit-done event received");
-                                // Additional handling for m2c2kit-done event if needed
-                            } else if (event.data.name === "m2c2kit-uploaded-data") {
-                                console.log("m2c2kit-uploaded-data event received");
-                                // Additional handling for m2c2kit-uploaded-data event if needed
-                            } else {
-                                console.log("Uncaught event received in parent window!: ");
-                                console.log(event);
-                            }
-                        });
-                        </script>';
-                    }
-                    
-                    
-                    
-                    //$this->includeJs('js/m2c2-redcap.js');
-                    //echo "calling enableM2C2('{$field['field_name']}')<br>";
-                    //echo '<script>$(document).ready(function() { enableM2C2("' . addslashes($field['field_name']) . '");});</script>';
+                    // Call the new function
+                    $this->processM2C2Field($field['field_name'], $m2c2Settings);
                 }
             }
         }
     }
 
+    // New function to process the M2C2 field
+    function processM2C2Field($field_name, $m2c2Settings) {
+        $m2c2SettingsJSON = json_encode($m2c2Settings);
+        
+        // Link format
+        // https://prod.m2c2kit.com/m2c2kit/nt/index.html
+        //      ?activity_name=[symbol-search, grid-memory, color-shapes, color-dots]
+        //      &api_key=demo
+        //      &study_id=demo
+        //      &number_of_trials=[NUM_TRIALS]::int
+        //      &width=400
+        //      &height=1000
+        //      &show_quit_button=false::boolean
+        //      &participant_id=None
+        //      &session_id=None
+        //      &admin_type=qualtrics
+
+        $url = 'https://prod.m2c2kit.com/m2c2kit/nt/index.html?';
+        $url .= 'activity_name=symbol-search';
+        $url .= '&api_key=demo&study_id=demo';
+        $url .= '&number_of_trials=' . $m2c2Settings['trials'] . "::int";
+        $url .= '&width=400';
+        $url .= '&height=1000';
+        $url .= '&show_quit_button=false::boolean';
+        $url .= '&participant_id=None';
+        $url .= '&session_id=None';
+        $url .= '&admin_type=qualtrics';
+
+        
+        echo '<div id="overlay-iframe-container" style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; z-index: 9999; background: rgba(0, 0, 0, 0.5);">
+            <iframe id="overlay-iframe" src="' . $url . '" style="height: 100%; width: 100%; border: none;"></iframe>
+        </div>';
+
+        echo '<script>
+        var m2c2Settings = ' . $m2c2SettingsJSON . ';
+        var embeddedDataPrefix = "TRIAL_DATA_";
+        window.addEventListener("message", function(event) {
+            console.log("event fired: " + event.data.name);
+            if (event.data.name === "m2c2kit-trial-done" || event.data.name === "newData") {
+                console.log("found new event: " + event.data.name);
+                console.log("data: " + event.data.data);
+                var data = JSON.parse(event.data.data);
+
+                // data will consist of multiple trials, but we want the last trial
+                var newData = data.trials[data.trials.length - 1];
+                var trial_num = newData.trial_index;
+
+                // Store data into embedded variable dynamically based on trial_num
+                var fieldName = m2c2Settings.trialdata[trial_num]; // Adjusted to start from 0 index
+                $("#" + fieldName).val(JSON.stringify(data));
+            } else if (event.data.name === "m2c2kit-done") {
+                console.log("m2c2kit-done event received");
+                
+                // Hide the iframe container
+                $("#overlay-iframe-container").hide();
+            }
+        });
+        </script>';
+    }
+    
     protected function includeJs($file) {
         // Use this function to use your JavaScript files in the frontend
         echo "Loading $file<br>";
